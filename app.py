e146ad265d731a6a7276f01c70b2e1f7067ab246
Francisco Neto <it.francisconeto@gmail.com>
2021-07-19 01:47:25 -0300

    detector de sorrisos

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ import cv2
 
 class Detectar_face(object):
     
-    def detectarFaceEmFoto(self, img, face_cascade):        
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray,1.1,4)
-        for(x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.imshow('img', img)
-        cv2.waitKey()
-
-    def detectarFaceEmCam(self, face_cascade):
+    def detectarFaceEmCam(self):
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        smile_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
         video_capture = cv2.VideoCapture(0)
         while True:
             ret, frame = video_capture.read()
@@ -19,9 +13,18 @@ class Detectar_face(object):
                 gray,
                 scaleFactor=1.1,
                 minNeighbors=5,
-                minSize=(30, 30)
+                minSize=(50, 50)
             )
             for (x, y, w, h) in faces:
+                faceROI = gray[y:y+h,x:x+w]
+                smile = smile_detector.detectMultiScale(
+                    faceROI, 
+                    3.5, 
+                    5
+                )
+                for (x3,y3,w3,h3) in smile:
+                    cv2.rectangle(frame,(x+x3,y+y3),(x+x3+w3,y+y3+h3),(0,0,255),3)
+                    print("ESTA SORRINDO!")
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cv2.imshow('Video', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -31,7 +34,4 @@ class Detectar_face(object):
 
 if __name__ == '__main__':    
     detectarFace = Detectar_face()
-    #img = cv2.imread('foto.jpg')    
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    #detectarFace.detectarFaceEmFoto(img, face_cascade)
-    detectarFace.detectarFaceEmCam(face_cascade)
+    detectarFace.detectarFaceEmCam()
